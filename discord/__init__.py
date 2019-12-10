DMcP89/discord.py

Remove useless SpeakingState enum and add Permissions-like variant
The SpeakingState enum I added in my vwsV4 pr was only used in one place as a default argument value.  This is technically a breaking change but oh well I hope no one actually used that thing.

discord/__init__.py
@@ -48,6 +48,7 @@ from .webhook import *
 from .voice_client import VoiceClient
 from .audit_logs import AuditLogChanges, AuditLogEntry, AuditLogDiff
 from .raw_models import *
+from .speaking_state import SpeakingState
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 

discord/gateway.py
@@ -39,7 +39,7 @@ import websockets
 
 from . import utils
 from .activity import _ActivityTag
-from .enums import SpeakingState
+from .speaking_state import SpeakingState
 from .errors import ConnectionClosed, InvalidArgument
 
 log = logging.getLogger(__name__)
@@ -687,7 +687,7 @@ class DiscordVoiceWebSocket(websockets.client.WebSocketClientProtocol):
 
         await self.send_as_json(payload)
 
-    async def speak(self, state=SpeakingState.voice):
+    async def speak(self, state=SpeakingState.speaking()):
         payload = {
             'op': self.SPEAKING,
             'd': {
@@ -728,8 +728,7 @@ class DiscordVoiceWebSocket(websockets.client.WebSocketClientProtocol):
             else:
                 user = vc._state.get_user(user_id)
 
-            vc._state.dispatch('voice_speaking_update', user, data['speaking'])
-
+            vc._state.dispatch('voice_speaking_update', user, SpeakingState(data['speaking']))
         elif op == self.CLIENT_CONNECT:
             self._connection._add_ssrc(int(data['user_id']), data['audio_ssrc'])
         elif op == self.CLIENT_DISCONNECT:

regulad/discord-ext-voice_recv

discord/__init__.py
@@ -52,6 +52,7 @@ from .voice_client import VoiceClient
 from .audit_logs import AuditLogChanges, AuditLogEntry, AuditLogDiff
 from .raw_models import *
 from .team import *
+from .speakingstate import SpeakingState
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 

discord/gateway.py
@@ -39,7 +39,7 @@ import websockets
 
 from . import utils
 from .activity import _ActivityTag
-from .enums import SpeakingState
+from .speakingstate import SpeakingState
 from .errors import ConnectionClosed, InvalidArgument
 
 log = logging.getLogger(__name__)
@@ -705,7 +705,7 @@ class DiscordVoiceWebSocket(websockets.client.WebSocketClientProtocol):
 
         await self.send_as_json(payload)
 
-    async def speak(self, state=SpeakingState.voice):
+    async def speak(self, state=SpeakingState.active()):
         payload = {
             'op': self.SPEAKING,
             'd': {
@@ -746,8 +746,7 @@ class DiscordVoiceWebSocket(websockets.client.WebSocketClientProtocol):
             else:
                 user = vc._state.get_user(user_id)
 
-            vc._state.dispatch('voice_speaking_update', user, data['speaking'])
-
+            vc._state.dispatch('voice_speaking_update', user, SpeakingState(data['speaking']))
         elif op == self.CLIENT_CONNECT:
             self._connection._add_ssrc(int(data['user_id']), data['audio_ssrc'])
         elif op == self.CLIENT_DISCONNECT: