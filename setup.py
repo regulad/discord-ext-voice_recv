020622faca64a2a573db6835a816f3960212ac25
Imayhaveborkedit <imayhaveborkedit@gmail.com>
2021-07-11 04:45:29 -0400

    Add requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 import re
 
-requirements = []
-with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
-
-version = ''
-with open('discord/__init__.py') as f:
+with open('discord/ext/voice_recv/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
@@ -20,58 +17,36 @@ if version.endswith(('a', 'b', 'rc')):
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += '+g' + out.decode('utf-8').strip()
+            version = version + out.decode('utf-8').strip()
     except Exception:
         pass
 
-readme = ''
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
-extras_require = {
-    'voice': ['PyNaCl>=1.3.0,<1.5'],
-    'docs': [
-        'sphinx==3.0.3',
-        'sphinxcontrib_trio==1.1.2',
-        'sphinxcontrib-websupport',
-    ]
-}
-
-setup(name='discord.py',
-      author='Rapptz',
-      url='https://github.com/Rapptz/discord.py',
-      project_urls={
-        "Documentation": "https://discordpy.readthedocs.io/en/latest/",
-        "Issue tracker": "https://github.com/Rapptz/discord.py/issues",
-      },
+setup(name='discord-ext-colours',
+      author='Imayhaveborkedit',
+      url='https://github.com/imayhaveborkedit/discord.py/tree/voice-recv-protocol',
       version=version,
-      packages=['discord', 'discord.ext.commands', 'discord.ext.tasks'],
+      packages=['discord.ext.voice_recv'],
       license='MIT',
-      description='A Python wrapper for the Discord API',
+      description='Experimental voice receive extension for discord.py',
       long_description=readme,
-      long_description_content_type="text/x-rst",
       include_package_data=True,
-      install_requires=requirements,
-      extras_require=extras_require,
-      python_requires='>=3.5.3',
+      python_requires='>=3.7',
+      install_requires=['discord.py[voice]>=1.7.3'],
+      extras_require=None,
+      zip_safe=False,
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
+        "Operating System :: POSIX",
+        "Operating System :: Windows",
+        "Operating System :: MacOS",
       ]
 )
